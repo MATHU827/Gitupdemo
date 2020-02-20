58426f57338e0d9b2417d623c8dc0f464c1eca22
BLTuser <BLTuser@BLT1204>
2020-02-20 12:03:45 +0530

    second commit

--- GradleDemo/src/test/java/DemoGradle/SampleGradle.java
+++ GradleDemo/src/test/java/DemoGradle/SampleGradle.java
@@ -1,5 +1,18 @@
 package DemoGradle;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class SampleGradle {
+	@Test
+	public void open()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\BLTuser.BLT1204\\Downloads\\driver\\chromedriver.exe");
+		 WebDriver driver = new ChromeDriver();
+			
+		
+		 driver.get("https://www.linkedin.com/");
+	}
 
 }
